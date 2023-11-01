13bd161c7613aabd0720c94cd38d8118219d6731
Mohammednihaljm <mohammednihaljm@gmail.com>
2023-11-02 00:09:43 +0530

    secondcommit:reduxtoolkit

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,13 +1,24 @@
 import { useSelector,useDispatch } from "react-redux";
+import {increase_count,dicrese_count} from "../redux/count/countSlice"
+
+
 function Counter() {
 const count=useSelector(state=>state.count.value)
+const dispatch=useDispatch()
+
+const increment=()=>{
+  dispatch(increase_count())
+}
+const dicrement=()=>{
+  dispatch(dicrese_count())
+}
 
     return (
       <div className="counter">
         <h2>Counter</h2>
         <h3>The count is -{count} </h3>
-        <button >Increase</button>
-        <button >Decrease</button>
+        <button onClick={increment}>Increase</button>
+        <button onClick={dicrement} >Decrease</button>
       </div>
     );
   }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,11 +2,12 @@ import { useSelector } from "react-redux";
 function Header() { 
 
   const color=useSelector(state=>state.color.value)
+  const count=useSelector(state=>state.count.value)
 
     return (
       <div className="header">
         <h1 style={{color}}>REDUX Project</h1>
-        <h3> the cart:</h3>
+        <h3> the cart:{count}</h3>
       </div>
     );
   }
